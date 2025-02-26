c1a5457589b331b095917cc2067222476721fafc
Asrar Ahammad <asrarahammadshaik@gmail.com>
2025-02-26 09:28:25 +0530

    About FS module in JS

---
+++ 02_fs/aboutPath.js
@@ -0,0 +1,10 @@
+import path from 'path'
+
+// console.log(path);
+
+let mypath = '/Users/shaikmohammadasrarahammad/Downloads/MyProjects/backend_practice/02_fs/aboutPath.js'
+
+console.log(path.extname(mypath));
+console.log(path.dirname(mypath));
+console.log(path.basename(mypath));
+
