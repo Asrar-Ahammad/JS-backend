7597c5785c211757d838f4e737ca5c19ab278506
Asrar Ahammad <asrarahammadshaik@gmail.com>
2025-02-27 22:03:08 +0530

    MongoDB exercise

---
+++ 11_mongodb_exercise/main.js
@@ -0,0 +1,13 @@
+//* Generate a dummy data in a collection called employees and a db called company
+
+/* {
+    name: "Harry",
+    salary:345123,
+    language:"Python",
+    ciyt:"delhi",
+    isManager:true
+}*/
+
+//* Generate 10 such records when a button called generate data is clicked!
+//* Create an Express app with mongoose to acheive it.
+//* Everytime the button is clicked, you should clear the collection.
